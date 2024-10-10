7ff0b32a897934e644471c403a13d5f5b3ee568b
정종환 <gksqlcxkdns1@naver.com>
2024-10-10 23:54:09 +0900

    쇠막대기 알고리즘 initialize

---
+++ stackQueue/쇠막대기/Main.java
@@ -0,0 +1,28 @@
+package stackQueue.쇠막대기;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+// 백준에서 class 는 Main 이여야함
+public class Main {
+
+    // 프로그래머스에서는 solution 메소드를 사용해야함
+    private static int solution(String str) {
+        // 결과 변수 선언
+        int answer = 0;
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // 막대기
+        String str = sc.next();
+
+        System.out.print(solution(str));
+    }
+}
