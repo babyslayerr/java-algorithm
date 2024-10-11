0b205ad009a62132b642a10b283c7be36c75ab44
정종환 <gksqlcxkdns1@naver.com>
2024-10-11 10:44:20 +0900

    쇠막대기, 레이저 알고리즘

--- stackQueue/쇠막대기/Main.java
+++ stackQueue/쇠막대기/Main.java
@@ -10,7 +10,19 @@ public class Main {
     private static int solution(String str) {
         // 결과 변수 선언
         int answer = 0;
-
+        Stack<Character> stack = new Stack<>();
+        for(int i =0; i<str.length();i++){
+            // 여는 괄호면 stack에 푸쉬
+            if(str.charAt(i) == '(') stack.push('(');
+            // 닫는 괄호
+            else {
+                stack.pop();
+                // 만약 이전 비교값이 여는 괄호면 레이저
+                if(str.charAt(i-1)=='(') answer+= stack.size();
+                // 1개의 막대의 끝이므로 1증가
+                else answer++;
+            }
+        }
 
         return answer;
     }
