73de66998de33cb1662548790b3ba3a8da0738f9
정종환 <gksqlcxkdns1@naver.com>
2024-10-14 10:13:56 +0900

    공주구하기 알고리즘 세팅

---
+++ stackQueue/공주구하기/Main.java
@@ -0,0 +1,26 @@
+package stackQueue.공주구하기;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+// 백준에서 class 는 Main 이여야함
+public class Main {
+
+    // 프로그래머스에서는 solution 메소드를 사용해야함
+    private static int solution(int[][] board, int[] moves) {
+        // 결과 변수 선언
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
