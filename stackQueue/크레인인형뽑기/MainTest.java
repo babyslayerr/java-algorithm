18e9bec721f8503976539a643760cc0cfca20b30
정종환 <gksqlcxkdns1@naver.com>
2024-10-26 10:46:41 +0900

    크레인 인형 뽑기 알고리즘 복습

---
+++ stackQueue/크레인인형뽑기/MainTest.java
@@ -0,0 +1,60 @@
+package stackQueue.크레인인형뽑기;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+// 백준에서 class 는 Main 이여야함
+public class MainTest {
+
+    // 프로그래머스에서는 solution 메소드를 사용해야함
+    private static int solution(int[][] board, int[] moves) {
+        // 결과 변수 선언
+        int answer = 0;
+
+        // 스택 변수 선언
+        Stack<Integer> stack = new Stack<>();
+
+        for(int x : moves){
+            for(int i = 0; i<board.length; i++){
+                if(board[i][x-1]!=0){
+                    // 값 창출
+                    int tmp = board[i][x-1];
+                    //뽑은 부분 0으로 초기화
+                    board[i][x-1]=0;
+                    if(!stack.isEmpty() && stack.peek() == tmp){
+                        // 팡
+                        answer += 2;
+                        stack.pop();
+                    }else stack.push(tmp);
+                    //하나 뽑았으므로 break
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // 격자 크기 n
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+        for(int i = 0 ; i<n; i++){
+            for(int j = 0; j<n; j++){
+                board[i][j] = sc.nextInt();
+            }
+        }
+        // moves 배열의 길이
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+        for(int i = 0; i<m;i++){
+             moves[i] = sc.nextInt();
+        }
+
+        System.out.print(solution(board,moves));
+    }
+}
