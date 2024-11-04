de7c423268adbb7abca8772b95b1be35c171a3df
정종환 <gksqlcxkdns1@naver.com>
2024-11-04 12:17:46 +0900

    마지막 쇠막대기

---
+++ stackQueue/쇠막대기/MainTest2.java
@@ -0,0 +1,39 @@
+package stackQueue.쇠막대기;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+// 백준에서 class 는 Main 이여야함
+public class MainTest2 {
+
+    // 프로그래머스에서는 solution 메소드를 사용해야함
+    private static int solution(String str) {
+        // 결과 변수 선언
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i<str.length() ; i++){
+            // 여는 괄호는 스택에 저장
+            if(str.charAt(i)=='(') stack.push('(');
+            else{
+                // 레이저거나 막대 끝(둘다 pop 이 필요)
+                stack.pop();
+                // 레이저면
+                if(str.charAt(i-1) == '(') answer+=stack.size();
+                // 막대 끝
+                else answer++;
+            }
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // 막대기
+        String str = sc.next();
+
+        System.out.print(solution(str));
+    }
+}
